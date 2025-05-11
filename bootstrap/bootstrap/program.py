9a9dd347bd5037712a463b53225e01dbb3175594
Somanath Mallela <130245231+mallelasomanath@users.noreply.github.com>
2025-05-11 10:48:20 +0530

    Add files via upload

---
+++ bootstrap/bootstrap/program.py
@@ -0,0 +1,9 @@
+subject=input("Enter your favourite subject:")
+if subject=="maths":
+    print("You are mpc student")
+elif subject=="physics":
+    print("You are mpc student")
+elif subject=="chemistry":
+    print("You are mpc student")
+else:
+    print(You are not a mpc student")
